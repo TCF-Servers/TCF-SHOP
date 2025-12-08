b98f8aa650ee362ac6de5fd1b6fe462141be97a5
Mathieu Trancoso <trancoso.mathieu@gmail.com>
2025-12-08 18:03:37 +0100

    push

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   skip_before_action :authenticate_user!, only: [:healthcheck]
   include Pundit::Authorization
 
-  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_authorized, except: [:index, :healthcheck], unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
   
   # Méthode pour le healthcheck (pour UptimeRobot)
