36e066c672e760b63aee1d9e23127391393f7ded
Mathieu Trancoso <trancoso.mathieu@gmail.com>
2026-02-23 09:36:18 +0100

    change activerecord queries

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,20 +6,19 @@ class PagesController < ApplicationController
   end
 
   def ranking
-    # Current month ranking - get all players with valid votes this month
+    # Current month ranking
     @current_month_ranking = Player.joins(:valid_votes)
-                                  .where(votes: { created_at: (Time.current.beginning_of_month - 2.hours)..(Time.current.end_of_month - 2.hours) })
-                                  .distinct
-                                  .sort_by(&:current_month_valid_votes)
-                                  .reverse
-                                  .first(50)
+                                  .where(votes: { created_at: Time.current.all_month })
+                                  .group("players.id")
+                                  .order("COUNT(votes.id) DESC")
+                                  .limit(50)
 
-    # Last month ranking - get all players with valid votes last month
+    # Last month ranking
+    last_month = Time.current.last_month
     @last_month_ranking = Player.joins(:valid_votes)
-                               .where(votes: { created_at: (Time.current.last_month.beginning_of_month - 2.hours)..(Time.current.last_month.end_of_month - 2.hours) })
-                               .distinct
-                               .sort_by(&:last_month_valid_votes)
-                               .reverse
-                               .first(50)
+                               .where(votes: { created_at: last_month.all_month })
+                               .group("players.id")
+                               .order("COUNT(votes.id) DESC")
+                               .limit(50)
   end
 end
